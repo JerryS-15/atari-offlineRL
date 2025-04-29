f0b33a44c752cfab7b2df2be0e4f3ee55ae0a9c9
JerryS-15 <JerryS15@outlook.com>
2025-04-29 14:50:05 +0200

    batch_size=256, context_size=50.

--- d3rlpy_testDT.py
+++ d3rlpy_testDT.py
@@ -91,8 +91,8 @@ def main() -> None:
     d3rlpy.envs.seed_env(env, args.seed)
 
     if args.game == "PongNoFrameskip-v4":
-        batch_size = 32  # 512
-        context_size = 10  # 50
+        batch_size = 256  # 512
+        context_size = 50  # 50
     else:
         batch_size = 128
         context_size = 30
