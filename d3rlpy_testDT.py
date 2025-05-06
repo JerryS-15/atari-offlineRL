57952d105ded00e7e21bbb8e214e45cc0b30ce21
JerryS-15 <JerryS15@outlook.com>
2025-05-07 00:59:05 +0200

    Rename path arg for file.

--- d3rlpy_testDT.py
+++ d3rlpy_testDT.py
@@ -144,7 +144,7 @@ def main(args) -> None:
 
     print("------------------------------")
     # dataset = toMDP(args)
-    with open(f"./d3Buffers/{args.env}_converted.h5", "rb") as f:
+    with open(f"./d3Buffers/{args.game}_converted.h5", "rb") as f:
         dataset = d3rlpy.dataset.ReplayBuffer.load(f, d3rlpy.dataset.InfiniteBuffer())
     print("Dataset Loaded.")
     print("------------------------------")
